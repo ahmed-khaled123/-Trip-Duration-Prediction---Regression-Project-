58911e1f9b52638a2d4731983bf47a9fffa48a21
ahmed-khaled123 <ahmedelbanna6969@gmail.com>
2025-08-23 23:59:13 +0300

    Add baseline Linear Regression model using modeling_Utils functions

--- src/models/KNN.py
+++ src/models/KNN.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from modeling_Utils.train_eval_test import  print_metrics, test_model
 from models.model_pipeline import run_model_pipeline
-from data.Data_helper import load_data
+from data_Utils.Data_helper import load_data
 
 # =================== LOAD DATA ===================
 train_df = load_data(r"G:\ML mostafa saad\slides\my work\13 Project 1 - Regression - Trip Duration Prediction\data\processed\train_processed_filtered.csv")
